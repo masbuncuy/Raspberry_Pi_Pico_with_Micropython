2b2dfd6d19d5fd68b4adfca91e537db086bd6ffb
Antonius Bun Wijaya <bunsmart18@gmail.com>
2023-05-12 19:53:33 +0700

    update code

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from machine import Pin, PWM
 from utime import sleep
 buzzer = PWM(Pin(1))
-buzzer.freq(500)
 
+#bisa diisi antara 10 - 12000
+buzzer.freq(1000)
+
+#untuk mengatur kenyaringan bisa diisi antara 0 - 1000
 buzzer.duty_u16(1000)
+
 sleep(1)
+
 buzzer.duty_u16(0)
+
 
