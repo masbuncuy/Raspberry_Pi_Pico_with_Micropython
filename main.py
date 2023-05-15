8cb75fa79717720573b4dae99dffed84a219b43f
Mas Bun Cuy <antoniusbunwijaya18052001@gmail.com>
2023-05-15 10:02:12 +0700

    up code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from machine import Pin
 import utime
 
+# deklarasi trigger, trriger untuk pengirim sinyal ultrasonic
 trigger = Pin(4, Pin.OUT)
+
+# deklarasi echo, echo sebagai penerima sinyal ultrasonic
 echo = Pin(5, Pin.IN)
 
 
@@ -11,11 +14,16 @@ def ultrasonic_sensor():
     trigger.high()
     utime.sleep_us(5)
     trigger.low()
+    # cek kondisi dari echo
     while echo.value() == 0:
+        # sinyal mati
         signaloff = utime.ticks_us()
     while echo.value() == 1:
+        # sinyal hidup
         signalon = utime.ticks_us()
+
     timepassed = signalon - signaloff
+    # jarak
     distance = (timepassed * 0.0343) / 2
     print("Jarak dari objek adalah ", distance, "cm")
 
