77d2517759e28fea0a72e398e83747068e97d6a4
Mas Bun Cuy <antoniusbunwijaya18052001@gmail.com>
2023-05-15 16:23:22 +0700

    up code

--- main.py
+++ main.py
@@ -6,8 +6,5 @@ while True:
     time.sleep(5)
     pin = Pin(3, Pin.OUT, Pin.PULL_DOWN)
     sensor = DHT11(pin)
-    t = (sensor.temperature)
-    h = (sensor.humidity)
-    print("Temperature: {}". format(sensor.temperature))
-    print("humidity: {}". format(sensor.humidity))
-
+    print("suhu: {}". format(sensor.temperature))
+    print("kelembapan: {}". format(sensor.humidity))
