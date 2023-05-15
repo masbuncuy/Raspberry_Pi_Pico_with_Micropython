439e183f9859fdf2921c6d9c652ce6d49ff3d329
Mas Bun Cuy <antoniusbunwijaya18052001@gmail.com>
2023-05-15 22:38:35 +0700

    update code

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from machine import Pin, I2C
 import utime as time
 from dht11 import DHT11
 
+data = Pin(3, Pin.OUT, Pin.PULL_DOWN)
+sensor = DHT11(data)
+
 while True:
     time.sleep(5)
-    pin = Pin(3, Pin.OUT, Pin.PULL_DOWN)
-    sensor = DHT11(pin)
     print("suhu: {}". format(sensor.temperature))
     print("kelembapan: {}". format(sensor.humidity))
